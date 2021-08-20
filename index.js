cdd1bd6209caf42e88810dfc6cc2190c666f5041
unknown <CoreyTyler>
2021-08-19 19:00:59 -0500

    route folders made

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const mysql = require('mysql');
-
-const db = mysql.createConnection({
-    host: '34.121.137.130',
-    user: 'root',
-    password: '12880758',
-    database: 'WW2Weapons'
-});
-
-db.connect((err) => {
-    if(err){
-        throw err;
-    }
-    console.log('mysql connected...');
-})
-
 const app = express();
-
 app.listen('3000', () => {
     console.log('server started on port 3000');
 });
+
+let pool = mysql.createPool({
+    connectionLimit: 100,
+    host: '34.121.137.130',
+    user: 'root',
+    password: '12880758',
+    database: 'WW2Weapons',
+    degug: false
+})
